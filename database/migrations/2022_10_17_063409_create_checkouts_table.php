3763d9995643107894cc7e746a9e226de94b4f0d
MetaTechVerse <yohanes7286@gmail.com>
2022-10-29 10:34:05 +0700

    Migration: users, camps, camp_benefit, checkouts

--- database/migrations/2022_10_17_063409_create_checkouts_table.php
+++ database/migrations/2022_10_17_063409_create_checkouts_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->date('expired');
             $table->string('cvc', 3);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
