52c41ed4cab926d515b7f541916aaa8304763e6f
MetaTechVerse <yohanes7286@gmail.com>
2022-11-19 14:49:10 +0700

    OAuth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,56 @@
-<!doctype html>
-<html lang="en">
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('styles/main.css')}}" type="text/css">
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-    <title>Laracamp by BuildWith Angga</title>
-</head>
+            <!-- Email Address -->
+            <div>
+                <x-label for="email" :value="__('Email')" />
 
-<body>
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
 
-    <section class="login-user">
-        <div class="left">
-            <img src="{{asset('images/ill_login_new.png')}}" alt="">
-        </div>
-        <div class="right">
-            <img src="{{asset('images/logo.png')}}" class="logo" alt="">
-            <h1 class="header-third">
-                Start Today
-            </h1>
-            <p class="subheader">
-                Because tomorrow become never
-            </p>
-            <p>
-                <a class="btn btn-border btn-google-login" href="#">
-                    <img src="{{asset('images/ic_google.svg')}}" class="icon" alt=""> Sign In with Google
-                </a>
-            </p>
-            <img src="{{asset('images/people.png')}}" class="people" alt="">
-        </div>
-    </section>
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
 
-    <!-- Optional JavaScript; choose one of the two! -->
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
+            </div>
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
 
-</body>
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
 
-</html>
+                <x-button class="ml-3">
+                    {{ __('Log in') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('index');
 })->name('index');
 
-Route::get('login', function () {
-    return view('auth.login');
-})->name('login');
+// Route::get('login', function () {
+//     return view('auth.user.login');
+// });
+
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/goggle/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
+
+Route::get('dashboard', function () {
+    return view('user.dashboard');
+})->name('dashboard');
 
 Route::get('checkout', function () {
     return view('checkout.create');
@@ -28,3 +25,5 @@ Route::get('checkout', function () {
 Route::get('success-checkout', function () {
     return view('checkout.success');
 })->name('success-checkout');
+
+require __DIR__.'/auth.php';
